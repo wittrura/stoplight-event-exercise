0c3757109035a0392e2c39671afd95e49144f224
Ryan Wittrup <ryan.wittrup@gmail.com>
2017-08-16 15:33:46 -0500

    Updates to MVC style to manage state, adds feature to toggle lights

--- index.js
+++ index.js
@@ -1,34 +1,81 @@
 (function() {
   'use strict';
 
-  function createToggle(btn, lightType, action) {
-    let speedBtn = document.getElementById(btn);
+  // model
+  let lightState = {
+    stopOn: false,
+    slowOn: false,
+    goOn: false
+  };
 
-    speedBtn.addEventListener('click', function(){
-      document.getElementById(lightType).classList.toggle(action);
-    });
+  // controller
 
-    speedBtn.addEventListener('mouseenter', function(){
-      console.log(`Entered ${this.textContent} button`);
-    });
+  function updateLightState(light) {
+    let lightOn = light.toLowerCase().concat('On');
 
-    speedBtn.addEventListener('mouseleave', function(){
-      console.log(`Left ${this.textContent} button`);
-    });
+    for (var key in lightState) {
+      if (key === lightOn) {
+        lightState[key] = !lightState[key];
+      } else {
+        lightState[key] = false;
+      }
+    }
+    console.log(lightState);
+    // based on model, add or remove classes to view
   }
 
-  // document.getElementById('stopButton').addEventListener('click', function(){
-  //   document.getElementById('stopLight').classList.toggle('stop');
-  // });
-  createToggle('stopButton', 'stopLight', 'stop');
+  function activateLights(bulbs) {
+    if (lightState.stopOn) {
+      // console.log(bulbs[0].id);
+      bulbs[0].classList.add('stop');
+      bulbs[1].classList.remove('slow');
+      bulbs[2].classList.remove('go');
+    } else if (lightState.slowOn) {
+      bulbs[0].classList.remove('stop');
+      bulbs[1].classList.add('slow');
+      bulbs[2].classList.remove('go');
+    } else if (lightState.goOn) {
+      bulbs[0].classList.remove('stop');
+      bulbs[1].classList.remove('slow');
+      bulbs[2].classList.add('go');
+    } else {
+      bulbs[0].classList.remove('stop');
+      bulbs[1].classList.remove('slow');
+      bulbs[2].classList.remove('go');
+    }
 
-  // document.getElementById('slowButton').addEventListener('click', function(){
-  //   document.getElementById('slowLight').classList.toggle('slow');
-  // });
-  createToggle('slowButton', 'slowLight', 'slow');
+  }
 
-  // document.getElementById('goButton').addEventListener('click', function(){
-  //   document.getElementById('goLight').classList.toggle('go');
-  // });
-  createToggle('goButton', 'goLight', 'go');
+  // function createToggle(btn, lightType, action) {
+  //   let speedBtn = document.getElementById(btn);
+  //
+  //   speedBtn.addEventListener('click', function(){
+  //     document.getElementById(lightType).classList.toggle(action);
+  //   });
+  //
+  //   speedBtn.addEventListener('mouseenter', function(){
+  //     console.log(`Entered ${this.textContent} button`);
+  //   });
+  //
+  //   speedBtn.addEventListener('mouseleave', function(){
+  //     console.log(`Left ${this.textContent} button`);
+  //   });
+  // }
+  //
+  // createToggle('stopButton', 'stopLight', 'stop');
+  // createToggle('slowButton', 'slowLight', 'slow');
+  // createToggle('goButton', 'goLight', 'go');
+
+
+  // view
+  document.getElementById('controls').addEventListener('click', function(e){
+    let target = e.target;
+
+    updateLightState(target.innerHTML);
+
+    let bulbs = document.getElementsByClassName('bulb');
+    activateLights(bulbs);
+
+    console.log(`${target.innerText} bulb on`)
+  });
 })();
