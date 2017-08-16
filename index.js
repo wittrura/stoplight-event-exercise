097f0c0aff01c662bf7293495e50ba2e05aae189
Ryan Wittrup <ryan.wittrup@gmail.com>
2017-08-16 14:10:50 -0500

    Adds listeners using constructor function

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 (function() {
   'use strict';
 
-  // YOUR CODE HERE
+  function createToggle(btn, light, action) {
+    document.getElementById(btn).addEventListener('click', function(){
+      document.getElementById(light).classList.toggle(action);
+    });
+  }
+
+  // document.getElementById('stopButton').addEventListener('click', function(){
+  //   document.getElementById('stopLight').classList.toggle('stop');
+  // });
+  createToggle('stopButton', 'stopLight', 'stop');
+
+  // document.getElementById('slowButton').addEventListener('click', function(){
+  //   document.getElementById('slowLight').classList.toggle('slow');
+  // });
+  createToggle('slowButton', 'slowLight', 'slow');
+
+  // document.getElementById('goButton').addEventListener('click', function(){
+  //   document.getElementById('goLight').classList.toggle('go');
+  // });
+  createToggle('goButton', 'goLight', 'go');
 })();
